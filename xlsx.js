cc0dff6a3a80da551eae670e79640079270a6794
deepak sharma <deepak.sharma@moglix.com>
2022-06-30 16:54:01 +0530

    xlsx data to json and save to mongosdb

--- xlsx.js
+++ xlsx.js
@@ -2,43 +2,64 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const multer = require('multer')
+const fs = require("fs")
+const { promisify } = require('util')
+const unlinkAsync = promisify(fs.unlink)
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/sample", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("connect with mongodb")
+}).catch((err) => {
+    console.log(err)
+})
 
 var XLSX = require("xlsx");
 
-
 XLSX.utils.sheet_to_json()
 
-
 const fileExtension = require('file-extension');
+const { json } = require('express')
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
 
 let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: (req, file, cb) => {
         cb(null, 'API/')
     },
-    filename: function (req, file, cb) {
+    filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, uniqueSuffix + '-' + file.originalname)
     }
 });
-
 const upload = multer({ storage: storage }).single('file');
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile("hello world");
 });
+const productSchema = new mongoose.Schema({
+    Name: String,
+    description: String,
 
-app.post('/upload', upload, (req, res) => {
+}, { strict: false })
+
+const Product = new mongoose.model("Product", productSchema)
+
+app.post('/upload', upload, async (req, res) => {
+    const product = await Product.create(req.body);
+
     let excel2json;
-
     console.log(req.file)
     const doc = XLSX.readFile(req.file.path);
     const json = XLSX.utils.sheet_to_json(doc.Sheets[doc.SheetNames[0]])
-    res.send(json);
-});
+    await Product.insertMany(json)
 
-app.listen('3000', function () {
+    await unlinkAsync(req.file.path)
+    res.send(json);
+
+    console.log(req.json)
+});
+app.listen('3000', () => {
     console.log('Server running on port 3000');
 });
